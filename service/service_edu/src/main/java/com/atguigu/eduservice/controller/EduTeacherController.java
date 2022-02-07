161ae687bd9bd681b7ff8c3dbcec1ddcf17c8861
ekiehuang <ekie729@gmail.com>
2022-02-07 15:12:41 -0800

    add the function of getting all teachers info

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,14 @@
 package com.atguigu.eduservice.controller;
 
 
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduTeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,8 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-02-07
  */
 @RestController
-@RequestMapping("/eduservice/edu-teacher")
+@RequestMapping("/eduservice/eduteacher")
 public class EduTeacherController {
+    @Autowired
+    private EduTeacherService teacherService;
 
+    @GetMapping
+    public List<EduTeacher> getAllTeacher() {
+        List<EduTeacher> list = teacherService.list(null);
+        return list;
+    }
 }
 
